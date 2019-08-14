e7ad10e8062ababa0ffcf310e876ed3ec312c828
Chihyun-Ahn <anchi15@naver.com>
2019-08-14 15:32:24 +0900

    20190814 commit

--- app.js
+++ app.js
@@ -15,20 +15,75 @@ exec("sudo ip link set can0 up type can bitrate 500000", function(err, stdout, s
 
 var can = require('socketcan');
 var fs = require('fs');
+var sensorLib = require('node-dht-sensor');
 
 // Parse database
-var network = can.parseNetworkDescription("./node_modules/socketcan/samples/can_definition_sample.kcd");
+var network = can.parseNetworkDescription("./node_modules/socketcan/samples/mycan_definition.kcd");
 var channel = can.createRawChannel("can0");
-var db_instr = new can.DatabaseService(channel, network.buses["Instrumentation"]);
-
+var db = new can.DatabaseService(channel, network.buses["FarmBUS"]);
 channel.start();
 
-// Register a listener to get any value changes
-db_instr.messages["TankController"].signals["TankTemperature"].onChange(function(s) {
-   console.log("Tank Temperature " + s.value);
-});
+var sensor = {
+   sensors: [
+      {
+         name: "House2Sen1",
+         type: 11,
+         pin: 2,
+         temperature: 0,
+         humidity: ""
+      },
+      {
+         name: "House2Sen2",
+         type: 11,
+         pin: 3,
+         temperature: 0,
+         humidity: ""
+      }
+   ],
+   read: function(){
+      for (var a in  this.sensors){
+         var b = sensorLib.read(this.sensors[a].type, this.sensors[a].pin);
+         this.sensors[a].temperature = b.temperature.toFixed(1);
+         this.sensors[a].humidity = b.humidity.toFixed(1);
+         console.log(
+            this.sensors[a].name + ": " +this.sensors[a].temperature + "°C, " + this.sensors[a].humidity + "%"
+         );
 
-// Register a listener to get any value updates
-// db_motor.messages["Emission"].signals["Enginespeed"].onUpdate(function(s) {
-//    console.log("Enginespeed " + s.value);
-// });
+      }
+   }
+};
+
+setInterval(function(){
+   sensor.read();
+   var sensorData = {
+      temperature1 : 0,
+      temperature2 : 0,
+      humidity1: "",
+      humidity2: ""
+   };
+
+   var controlData = {
+      fan1: 0,
+      fan2: 0,
+      fan3: 0,
+      feed: 0,
+      water: 0,
+      alarm: 0
+   };
+
+   sensorData.temperature1 = sensor.sensors[0].temperature;
+   sensorData.temperature2 = sensor.sensors[1].temperature;
+   sensorData.humidity1 = sensor.sensors[0].humidity;
+   sensorData.humidity2 = sensor.sensors[1].humidity;
+
+   db.messages["House2Stat"].signals["temperature1"].update(sensorData.temperature1);
+   db.messages["House2Stat"].signals["temperature2"].update(sensorData.temperature2);
+   db.messages["House2Stat"].signals["humidity1"].update(sensorData.humidity1);
+   db.messages["House2Stat"].signals["humidity2"].update(sensorData.humidity2);
+
+   //Trigger sending this message
+   db.send("House2Stat");
+
+}, 6000);
+
+
